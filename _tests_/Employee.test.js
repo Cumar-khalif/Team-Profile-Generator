ca71421e6e7838041492eb5f192e3b9760262b86
omar khalif <73026620+Cumar-khalif@users.noreply.github.com>
2021-10-26 10:34:41 -0500

    clearing up the errors

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,45 @@
+const Employee = require('../lib/employee.js');
+
+test('A test for creating a new Employee instance', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(typeof(newEmployee)).toBe("object");
+})
+
+// TEST PROPERTIES
+
+test('A test for setting name for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.name).toBe("Test");
+})
+
+test('A test for setting id for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.id).toBe(1);
+})
+
+test('A test for setting email for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.email).toBe('test@email.com');
+})
+
+// TEST methods
+
+test('A test for getting name for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.getName()).toBe('Test');
+})
+
+test('A test for getting id for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.getId()).toBe(1);
+})
+
+test('A test for getting email for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.getEmail()).toBe('test@email.com');
+})
+
+test('A test for getting role for new Employee', () => {
+    const newEmployee = new Employee('Test', 1, 'test@email.com');
+    expect(newEmployee.getRole()).toBe("Employee");
+})
